db8c6092253b4d6f0e5d028ef357271cf7a67eee
priyansusatote <priyansusatote@gmail.com>
2026-06-12 19:50:18 +0530

    OpenAPI and Swagger UI:  for workflow engine APIs

--- src/main/java/com/priyansu/workflow/security/SecurityConfig.java
+++ src/main/java/com/priyansu/workflow/security/SecurityConfig.java
@@ -30,6 +30,11 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
